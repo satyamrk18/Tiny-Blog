27bb16518e332b3836d0fcb680236dca5a91ad80
Satyamrk <satyamrk18@gmail.com>
2025-10-04 23:14:52 +0530

    style: basic card style added

--- client/src/Components/BlogCard.jsx
+++ client/src/Components/BlogCard.jsx
@@ -1,24 +1,41 @@
 import { Link } from "react-router";
-const BlogCard = ({ _id, author, title,subtitle,thumbnail, category, content, publish_at,slug }) => {
+const BlogCard = ({
+  _id,
+  author,
+  title,
+  subtitle,
+  thumbnail,
+  category,
+  content,
+  publish_at,
+  slug,
+}) => {
   return (
     <div
-      className="border-2 m-auto my-5 p-3 w-[50%] flex flex-col items-start"
+      className="border-2 m-auto my-5 p-3 w-[50%] flex flex-row items-center justify-between"
       key={_id}
     >
-      <h3 className="">
+      <div className="flex flex-col">
+        <h3 className="">
         <span className="text-3xl rounded-full">
           {author?.name.substring(0, 1)}{" "}
         </span>
         {author?.name}
       </h3>
-      <Link to={`/blog/${slug}`}><h2>{title}</h2></Link>
+      <Link to={`/blog/${slug}`}>
+        <h2>{title}</h2>
+      </Link>
       <p>{subtitle}</p>
       <h4>{category}</h4>
       <p>{content}</p>
       <p className="border-2 rounded-xl p-1 text-sm bg-gray-50 ">
         {publish_at.split("T")[0]}
       </p>
-      <img src={thumbnail[0]} alt="thumbnail-img"/>
+      </div>
+
+      <div>
+          {thumbnail[0] ? <img src={thumbnail[0]} alt="thumbnail-img" className="" /> : ""}
+      </div>
     </div>
   );
 };
