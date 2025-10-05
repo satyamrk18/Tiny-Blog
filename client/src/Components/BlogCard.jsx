1149d25f7da1a07ea3fc977312b9a35e933bd611
Satyamrk <satyamrk18@gmail.com>
2025-10-05 18:02:56 +0530

    style: BlogCard styling

--- client/src/Components/BlogCard.jsx
+++ client/src/Components/BlogCard.jsx
@@ -12,28 +12,29 @@ const BlogCard = ({
 }) => {
   return (
     <div
-      className="border-1 rounded-2xl m-auto my-5 p-3 w-3xs flex flex-col items-start justify-between"
+      className="bg-white rounded-2xl m-auto my-5 w-3xs flex flex-col items-start justify-between "
       key={_id}
     >
-       <p className="">
+        <div>
+          {thumbnail[0] ? <img src={thumbnail[0]} alt="thumbnail-img" className="w-screen h-[150px] rounded-t-xl" /> : ""}
+      </div>
+      <p className="pl-2">
         <span className="text-3xl rounded-full">
           {author?.name.substring(0, 1)}{" "}
         </span>
         {author?.name}
       </p>
-        <div>
-          {thumbnail[0] ? <img src={thumbnail[0]} alt="thumbnail-img" className="w-screen h-[150px]" /> : ""}
-      </div>
-      <div className="flex flex-col">
+      <div className="flex flex-col p-2 gap-2">
       <Link to={`/blog/${slug}`}>
         <p className="font-bold text-xl">{title}</p>
       </Link>
-      <p className="text-lg">{subtitle}</p>
-      <h4>{category}</h4>
-      <p>{content}</p>
-      <p className="border-2 rounded-xl p-1 text-sm bg-gray-50 ">
+      <p className="text-sm text-gray-500">{subtitle}</p>
+     <div className="flex items-baseline justify-between">
+       <h4>{category}</h4>
+      <p className="border-1 rounded-xl w-fit p-1 text-xs bg-gray-50 ">
         {publish_at.split("T")[0]}
       </p>
+     </div>
       </div>
     </div>
   );
