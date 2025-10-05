e7dbeeee43e01dba6ff1eb78f30f06285985213f
Satyamrk <satyamrk18@gmail.com>
2025-10-05 20:44:50 +0530

    perticular blog data retirve by slug

--- client/src/Views/ReadBlog.jsx
+++ client/src/Views/ReadBlog.jsx
@@ -1,9 +1,23 @@
-const ReadBlog = ()=>
-{
-    return(
-        <div>
-            <h1>Read Blog</h1>
-        </div>
-    )
-}
+import { useState, useEffect } from "react";
+import { useParams } from "react-router";
+import SimpleMDE from "react-simplemde-editor";
+import axios from "axios";
+const ReadBlog = () => {
+  const [blog, setBlog] = useState("");
+
+  const { slug } = useParams();
+
+  const loadBlog = async () => {
+    const response = await axios.get(
+      `${import.meta.env.VITE_SERVER_URL}/blog/${slug}`
+    );
+    setBlog(response.data.data);
+  };
+  useEffect(()=>{loadBlog()},[],)
+  return (
+    <div>
+      <h1>{blog.title}</h1>
+    </div>
+  );
+};
 export default ReadBlog;
